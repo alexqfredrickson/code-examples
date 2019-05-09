0849d73e51fd191ffc7ddf46e1a20670c7d0493b
aqf <alexqfredrickson@gmail.com>
2019-05-09 15:21:47 -0400

    articles api is now functional

--- aspnet-core-2.2-poc/API/articles.cs
+++ aspnet-core-2.2-poc/API/articles.cs
@@ -1,50 +1,78 @@
 using System;
 using System.IO;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Data.SQLite;
+using System.Data.Common;
 using System.Reflection;
 using static System.IO.DirectoryInfo;
-
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace aspnetcoreapp.API
 {
 
-    public class ArticleContext : DbContext
-    {
-        public string ParentDirectoryPath() 
-        {
-            DirectoryInfo executingAssembly = new DirectoryInfo(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            DirectoryInfo executingAssemblyParentDirectory = executingAssembly.Parent;
-            return executingAssemblyParentDirectory.Name;
-        }
-
-        public SQLiteConnection SqliteConnection() {
-            AppDomain.CurrentDomain.SetData("DataDirectory", this.ParentDirectoryPath());
-            return new System.Data.SQLite.SQLiteConnection("Data Source=|DataDirectory|/database/db.sqlite");        
-        }
-    }
-
-    [Route("api/[controller]")]
+    [Route("api/articles/get")]
     [ApiController]
     public class ArticleController : ControllerBase
     {
-        private readonly ArticleContext _context;
-
-        public ArticleController(ArticleContext context)
+        private string ParentDirectoryPath() 
         {
-            _context = context;
+            // note: this is ugly and in the real world it would be hard-coded in a config file
+            return Directory.GetParent(Directory.GetParent(Directory.GetParent(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+            ).ToString()).ToString()).ToString();
+        }
 
-            // if (_context.TodoItems.Count() == 0)
-            // {
-            //     // Create a new TodoItem if collection is empty,
-            //     // which means you can't delete all TodoItems.
-            //     _context.TodoItems.Add(new TodoItem { Name = "Item1" });
-            //     _context.SaveChanges();
-            // }
+        private SQLiteConnection SqliteConnection() {
+            return new System.Data.SQLite.SQLiteConnection(
+                String.Format("Data Source={0}/database/db.sqlite", this.ParentDirectoryPath())
+            );        
+        }
+
+        public string GetArticles()
+        {
+            var articles = new List<Object>();
+
+            Console.Write(this.SqliteConnection().ToString());
+
+            using (var s = this.SqliteConnection()) {
+
+                Console.WriteLine("ERROR: The connection string is invalid: " + s.ConnectionString);
+
+                try 
+                {
+                    s.Open();
+                }
+                catch 
+                {
+                    Console.WriteLine("ERROR: The connection string is invalid: " + s.ConnectionString);
+                }
+
+                string getArticlesSqlStatement = "SELECT * FROM articles LIMIT 20";
+
+                System.Data.SQLite.SQLiteCommand sqlCommand = new SQLiteCommand(getArticlesSqlStatement, s);
+                System.Data.SQLite.SQLiteDataReader sqliteDataReader = sqlCommand.ExecuteReader();
+
+                while (sqliteDataReader.Read()) 
+                {
+                    articles.Add(
+                        new {
+                            Id = sqliteDataReader[0].ToString(),             
+                            Title = sqliteDataReader[1].ToString(),                                
+                            Url = sqliteDataReader[2].ToString(),                            
+                            Points = sqliteDataReader[3].ToString(),                            
+                            TimeCreated = sqliteDataReader[4].ToString()                                           
+                        }
+                    );
+                }
+
+                s.Close();
+            }
+
+            return JsonConvert.SerializeObject(articles, Formatting.Indented);
         }
     }
 }
