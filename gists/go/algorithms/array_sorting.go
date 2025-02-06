3dfdb20428bc4ede21d0c0956179da05c242c93e
Alex <alex.q.fredrickson@gmail.com>
2025-02-06 14:12:22 -0500

    array sorting in go

---
+++ gists/go/algorithms/array_sorting.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+)
+
+func quick_sort(slice []int) []int {
+	if len(slice) == 0 {
+		return nil
+	}
+
+	if len(slice) == 1 {
+		return slice
+	}
+
+	// pick a pivot point from the middle
+	pivot_index := int(float64(len(slice) / 2))
+	pivot := slice[pivot_index]
+
+	var smaller_or_equal_numbers []int
+	var bigger_numbers []int
+
+	for i := 0; i <= len(slice)-1; i++ {
+		if i == pivot_index {
+			continue
+		}
+
+		if slice[i] <= pivot {
+			smaller_or_equal_numbers = append(smaller_or_equal_numbers, slice[i])
+		} else {
+			bigger_numbers = append(bigger_numbers, slice[i])
+		}
+	}
+
+	// move quick-sorted smaller numbers to the left of the pivot
+	new_slice := append(quick_sort(smaller_or_equal_numbers), pivot)
+
+	// move quick-sorted bigger numbers to the right of the pivot
+	return append(new_slice, quick_sort(bigger_numbers)...)
+}
+
+func merge_sort() {
+	// todo
+}
+
+func heap_sort() {
+	// todo
+}
+
+func counting_sort() {
+	// todo
+}
+
+func radix_sort() {
+	// todo
+}
+
+func bubble_sort() {
+	// todo
+}
+
+func selection_sort() {
+	// todo
+}
+
+func insertion_sort() {
+	// todo
+}
+
+func shell_sort() {
+	// todo
+}
+
+func bucket_sort() {
+	// todo
+}
+
+func main() {
+	a := []int{15, 12, 9, 8, 3, 1, 2, 4, 6, 5, 11, 7, 10, 13, 14}
+
+	fmt.Println(quick_sort(a))
+	// fmt.Println(merge_sort(a))
+	// fmt.Println(heap_sort(a))
+	// fmt.Println(counting_sort(a))
+	// fmt.Println(radix_sort(a))
+	// fmt.Println(bubble_sort(a))
+	// fmt.Println(selection_sort(a))
+	// fmt.Println(insertion_sort(a))
+	// fmt.Println(shell_sort(a))
+	// fmt.Println(bucket_sort(a))
+
+}
--- gists/go/project_euler/go.mod
+++ gists/go/project_euler/go.mod
@@ -1,3 +1,3 @@
 module project_euler
 
-go 1.14
+go 1.23.6
